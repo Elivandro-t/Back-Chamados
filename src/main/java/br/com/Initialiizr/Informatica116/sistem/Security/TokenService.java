cc20662dae1ff94ed2cc38db6cddcd3f541d9df9
elivandro <elivandro78@gmail.com>
2024-05-27 20:22:54 -0300

    diminuindo expiration token

--- src/main/java/br/com/Initialiizr/Informatica116/sistem/Security/TokenService.java
+++ src/main/java/br/com/Initialiizr/Informatica116/sistem/Security/TokenService.java
@@ -40,7 +40,7 @@ public class TokenService {
     }
 
     private Instant DataExpired() {
-        return LocalDateTime.now().minusHours(1)
+        return LocalDateTime.now().minusHours(2)
                 .toInstant(ZoneOffset.of("-03:00"));
 
     }
