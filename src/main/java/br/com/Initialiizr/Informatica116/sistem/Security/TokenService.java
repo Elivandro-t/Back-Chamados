d56055a05693e97300559c7d689e9147cbffa1d8
elivandro <elivandro78@gmail.com>
2024-05-29 16:25:25 -0300

    adicionando horas

--- src/main/java/br/com/Initialiizr/Informatica116/sistem/Security/TokenService.java
+++ src/main/java/br/com/Initialiizr/Informatica116/sistem/Security/TokenService.java
@@ -40,7 +40,7 @@ public class TokenService {
     }
 
     private Instant DataExpired() {
-        return LocalDateTime.now().plusMinutes(1)
+        return LocalDateTime.now().plusHours(1)
                 .toInstant(ZoneOffset.of("-03:00"));
 
     }
