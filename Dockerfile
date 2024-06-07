2141dd794fe723d8f33bde2f4a785f4ef6e579dc
elivandro <elivandro78@gmail.com>
2024-06-07 00:53:19 -0300

    add

--- Dockerfile
+++ Dockerfile
@@ -3,20 +3,16 @@ FROM ubuntu:latest as build
 RUN apt-get update && apt-get install -y \
     openjdk-17-jdk \
     maven
-
 WORKDIR /apt
 COPY . .
 RUN mvn clean install
 
 FROM openjdk:17-jdk-slim
-
+VOLUME /apt
 VOLUME /apt/Logos
 VOLUME /apt/Img
 VOLUME /apt/sistemBotao
-
 EXPOSE 8080
-
-WORKDIR /apt
 COPY --from=build /apt/target/Informatica-0.0.1-SNAPSHOT.jar /apt/app.jar
 COPY --from=build /apt/Logos /apt/Logos
 COPY --from=build /apt/Img /apt/Img
--- src/main/java/br/com/Initialiizr/Informatica116/sistem/repository/UserRepository.java
+++ src/main/java/br/com/Initialiizr/Informatica116/sistem/repository/UserRepository.java
@@ -3,10 +3,12 @@ package br.com.Initialiizr.Informatica116.sistem.repository;
 import br.com.Initialiizr.Informatica116.sistem.Models.AUTH_USER.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 @Repository
+@EnableJpaRepositories
 public interface UserRepository extends JpaRepository<User,Long> {
     @Query("select s from User s where s.email = :email")
     Optional pegandoUsuarioExistente(String email);
