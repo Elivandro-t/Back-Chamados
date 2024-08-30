cf3589f15e6829c5e1a3523bedb5e154e84d5780
elivandro <elivandro78@gmail.com>
2024-08-30 11:41:38 -0300

    adicionando correcao

--- src/main/java/br/com/Initialiizr/Informatica116/sistem/validators/CriaChamadoAuto.java
+++ src/main/java/br/com/Initialiizr/Informatica116/sistem/validators/CriaChamadoAuto.java
@@ -52,7 +52,7 @@ public class CriaChamadoAuto {
         object.put("itens", jsonArray);
         String jsonString = object.toString(4);
             var Hora = LocalDateTime.now();
-            var manha = Hora.getHour() == 10 && Hora.getMinute() == 30;
+            var manha = Hora.getHour() == 8 && Hora.getMinute() == 30;
             var domingo = Hora.getDayOfWeek().equals(DayOfWeek.SUNDAY);
             var tarde = Hora.getHour() == 14 && Hora.getMinute() == 2;
 
@@ -60,7 +60,7 @@ public class CriaChamadoAuto {
                     chamadoService.registrar(jsonString, null);
                     chamado = true;
                 } else if (tarde ) {
-                    
+                    chamadoService.registrar(jsonString, null);
                 }
         }catch (RuntimeException e){
 
